260c74474685b185f0a0e636c0fc9f0448c879df
Javier Garrayo <jgarrayo@bssclife020.bsc.es>
2018-06-15 10:52:27 +0200

    removed TimeOut in JS code

--- JS/trial.js
+++ JS/trial.js
@@ -18,72 +18,32 @@ loadurl = function (){
                 "https://dev-openebench.bsc.es/api/scientific/Dataset/QfO:QfO4_STD_Fungi_InParanoidCore_output.json"
               ]
     // var urls = ['https://rawgit.com/inab/benchmarking-data-model/master/prototype-data/QfO_complete_data/ECtest_EggNOG_output.json']
-    let array = get_data(urls)
-    .then(
-      function(array){
-        console.log(array)
-        let full_json = join_all_json(array);
-        // console.log(full_json)
-
-        // Promise.resolve(full_json).then(function (res) {
-        //   // createChart(res)
-        //   console.log(res);
-        // });
-        setTimeout(() => {
-          createChart(full_json)
-        }, 200);
-          
-          
-        
-        
-      }
-    )
+    get_data(urls)
+  
        
 };
 
-async function get_data(urls){
-  var array = [];
-  urls.forEach(url => {
-    array.push(fetchUrl(url));
-  });
-  return (array)
-}
-
-function join_all_json(array){
+function get_data(urls){
+  Promise.all(urls.map(url =>
+    fetch(url)
+    .then(resp => resp.json())
+  )).then(results => {
+    join_all_json(results)
+  })
+ };
+ 
+ function join_all_json(array){
   let full_json  = [];
-
   for (var i = 0; i < array.length; i++) {
-    Promise.resolve(array[i])
-    .then(function(result) {
-      // console.log(result);
-      var jo = {};
-      jo['toolname'] = result.name.split('.')[0];
-      jo['x'] = result.metrics[0].result.value;
-      jo['y'] = result.metrics[1].result.value;
-      jo['e'] = result.metrics[2].result.value;
-      full_json.push(jo)
-    });
+      let jo = {};
+      jo['toolname'] = array[i].name.split('.')[0];
+      jo['x'] = array[i].metrics[0].result.value;
+      jo['y'] = array[i].metrics[1].result.value;
+      jo['e'] = array[i].metrics[2].result.value;
+      full_json.push(jo)    
   }
-
-  // console.log(full_json)
-  return(full_json);
-
-    
-}
-
-async function fetchUrl(url) {
-    try {
-
-        let request = await fetch(url);
-        let result = await request.text();
-
-        return JSON.parse(result);
-    }
-    catch (err) {
-        console.log(`Invalid Url Error: ${err.stack} `);
-}
-}
-  
+  createChart(full_json);    
+ }
   
 createChart = function (data){
 
@@ -100,7 +60,7 @@ createChart = function (data){
 
   var xScale = d3.scaleLinear()
         .range([0, width])
-        .domain([d3.min(data, function(d) { console.log(d.x); return d.x; }), d3.max(data, function(d) { return d.x; })]).nice();
+        .domain([d3.min(data, function(d) { return d.x; }), d3.max(data, function(d) { return d.x; })]).nice();
 
   var min_y = d3.min(data, function(d) { return d.y; });
   var max_y = d3.max(data, function(d) { return d.y; });
@@ -242,12 +202,10 @@ createChart = function (data){
     .append("path");
     
   dots.attr("d", symbol.type(function(){return d3.symbolSquare}))
-  .attr("id", function (d) { console.log(d.toolname.replace(/[\. ()/-]/g, "_")); return d.toolname.replace(/[\. ()/-]/g, "_");})
+  .attr("id", function (d) {  return d.toolname.replace(/[\. ()/-]/g, "_");})
   .attr("class","line")
   .attr('transform',function(d){ return "translate("+xScale(d.x)+","+yScale(d.y)+")"; })
   .attr("r", 6)
-
-
   // .style("fill", function (d){return "#" + Math.random().toString(16).slice(2, 8)})
   .style("fill", function(d) {
     return color(cValue(d));
@@ -265,27 +223,8 @@ createChart = function (data){
           .duration(1000)		
           .style("opacity", 0);	
   });
-  // [quantile_x, quantile_y] = get_square_quartiles(data);
-  // var x_axis = xScale.domain();
-  // var y_axis = yScale.domain();
-
-  // svg.append("line")
-  //       .attr("x1", xScale(quantile_x))
-  //       .attr("y1", yScale(y_axis[0]))
-  //       .attr("x2", xScale(quantile_x))
-  //       .attr("y2", yScale(y_axis[1]))
-  //       .attr("stroke", "black")
-  //       .style("stroke-dasharray", ("3, 3"))
-  //       .style("opacity", 0.5);
-
-  // svg.append("line")
-  //   .attr("x1", xScale(x_axis[0]))
-  //   .attr("y1", yScale(quantile_y))
-  //   .attr("x2", xScale(x_axis[1]))
-  //   .attr("y2", yScale(quantile_y))
-  //   .attr("stroke", "black")
-  //   .style("stroke-dasharray", ("3, 3"))
-  //   .style("opacity", 0.5);
+  get_square_quartiles(data, svg, xScale, yScale);
+  
 
         
   // --------------------------------------- 
@@ -310,6 +249,8 @@ createChart = function (data){
   .style("fill", color)
   .on('click', function(d) {
     //console.log("dots",dots)
+    d3.select("#x_quartile").style("opacity", 0);
+    d3.select("#y_quartile").style("opacity", 0);
     dot = d3.select("text#" +d.replace(/[\. ()/-]/g, "_"))
     var ID = dot._groups[0][0].id
 
@@ -335,43 +276,6 @@ createChart = function (data){
     }
 
       
-    
-    //  ID.active = active;
-      
-    //  var active2   = ("top"+ID).active ? false : true,
-    //  newOpacity2 = active2 ? 0 : 1;
-    //   // hide or show the elements
-    //   d3.select("#top"+ID).style("opacity", newOpacity2);
-    //   // update whether or not the elements are active
-    //   ("top"+ID).active = active2;
-
-    //  var active3  = ("bottom"+ID).active ? false : true,
-    //  newOpacity3 = active3 ? 0 : 1;
-    //   // hide or show the elements
-    //   d3.select("#bottom"+ID).style("opacity", newOpacity3);
-    //   // update whether or not the elements are active
-    //   ("bottom"+ID).active = active3;
-
-    //   var active4  = ("line"+ID).active ? false : true,
-    //  newOpacity4 = active3 ? 0 : 1;
-    //   // hide or show the elements
-    //   d3.select("#line"+ID).style("opacity", newOpacity4);
-    //   // update whether or not the elements are active
-    //   ("line"+ID).active = active4;
-
-    
-
-    // div.transition()		
-    // .duration(100)		
-    // .style("opacity", .9);		
-    // div.html(dot_data.__data__.toolname + "<br/>"  + formatComma(dot_data.__data__.x) + "<br/>"  + formatDecimal(dot_data.__data__.y))	
-    // .style("left", coordinates[0] + "px")     
-    // .style("top", coordinates[1] + "px");
-  })
-  .on("mouseout", function(d) {		
-    div.transition()		
-        .duration(1000)		
-        .style("opacity", 0);	
   });
 
 
@@ -391,14 +295,61 @@ createChart = function (data){
 
 
 
-function get_square_quartiles(data) {
+function get_square_quartiles(data, svg, xScale, yScale) {
   var x_values = data.map(a => a.x).sort(function(a, b){return a - b});
   var y_values = data.map(a => a.y).sort(function(a, b){return a - b});
 
   var quantile_x = d3.quantile(x_values, 0.5);
   var quantile_y = d3.quantile(y_values, 0.5);
 
-  return [quantile_x, quantile_y];
+  var x_axis = xScale.domain();
+  var y_axis = yScale.domain();
+
+  svg.append("line")
+        .attr("x1", xScale(quantile_x))
+        .attr("y1", yScale(y_axis[0]))
+        .attr("x2", xScale(quantile_x))
+        .attr("y2", yScale(y_axis[1]))
+        .attr("id", function (d) { return "x_quartile";})
+        .attr("stroke", "black")
+        .style("stroke-dasharray", ("3, 3"))
+        .style("opacity", 0.5)
+        .on("mouseover", function(d) {	
+          div.transition()		
+              .duration(100)		
+              .style("opacity", .9);		
+          div	.html(formatComma(quantile_x) )	
+              .style("left", (d3.event.pageX) + "px")		
+              .style("top", (d3.event.pageY) + "px");
+          })					
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(1000)		
+                .style("opacity", 0);	
+        });
+
+  svg.append("line")
+      .attr("x1", xScale(x_axis[0]))
+      .attr("y1", yScale(quantile_y))
+      .attr("x2", xScale(x_axis[1]))
+      .attr("y2", yScale(quantile_y))
+      .attr("id", function (d) { return "y_quartile";})
+      .attr("stroke", "black")
+      .style("stroke-dasharray", ("3, 3"))
+      .style("opacity", 0.5)
+      .on("mouseover", function(d) {	
+        div.transition()		
+            .duration(100)		
+            .style("opacity", .9);		
+        div	.html(formatComma(quantile_y) )	
+            .style("left", (d3.event.pageX) + "px")		
+            .style("top", (d3.event.pageY) + "px");
+        })					
+      .on("mouseout", function(d) {		
+          div.transition()		
+              .duration(1000)		
+              .style("opacity", 0);	
+      });
 };
 
   
