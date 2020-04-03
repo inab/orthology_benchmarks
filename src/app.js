ff336e9c8a43d83ec2a5f238b8efdb7f3f1e11cc
Vicky Sundesha <me.vicky@hotmail.com>
2020-04-03 18:18:28 +0200

    saving download svg only works

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import './app.css';
 import { createApolloFetch } from 'apollo-fetch';
 import { append_classifiers_list } from './selection_list';
 import { createChart } from './scatter_plot';
-// import { saveAsImage } from './saveImageAsPng';
-import * as svg from 'save-svg-as-png';
+import { svg } from 'd3';
+//import { saveAsImage } from './saveImageAsPng';
+import * as svgtopng from 'save-svg-as-png';
 
 // ./node_modules/.bin/webpack-cli src/app.js --output=build/build.js -d -w
 
@@ -54,13 +55,7 @@ function load_scatter_visualization() {
 		//check the transformation to table attribute and append table to html
 		if (y.getAttribute('toTable') == 'true') {
 			let table_id = divid + '_table';
-			var input = $(
-				'<br><br><table id="' +
-					table_id +
-					'" data-id="' +
-					dataId +
-					'" class="benchmarkingTable_scatter"></table>'
-			);
+			var input = $('<br><br><table id="' + table_id + '" data-id="' + dataId + '" class="benchmarkingTable_scatter"></table>');
 			$('#' + divid + 'flex-container').append(input);
 		}
 
@@ -69,32 +64,31 @@ function load_scatter_visualization() {
 }
 
 function downloadPng(name, id) {
-	svg.saveSvgAsPng(document.getElementById(id), name + '.png', {
-		backgroundColor: 'white'
+	console.log(name, id);
+	svgtopng.saveSvgAsPng(document.getElementById(id), name + '.png', {
+		backgroundColor: 'white',
 	});
 }
 
 function get_data(url, json_query, dataId, divid) {
 	try {
 		const fetch = createApolloFetch({
-			uri: url
+			uri: url,
 		});
 
 		let vars = { dataset_id: dataId };
 
 		fetch({
 			query: json_query,
-			variables: vars
-		}).then(res => {
+			variables: vars,
+		}).then((res) => {
 			let result = res.data.getDatasets;
 			if (result.length == 0) {
 				document.getElementById(divid + '_dropdown_list').remove();
 
 				var para = document.createElement('td');
 				para.id = 'no_benchmark_data';
-				var err_txt = document.createTextNode(
-					'No data available for the selected challenge: ' + dataId
-				);
+				var err_txt = document.createTextNode('No data available for the selected challenge: ' + dataId);
 				para.appendChild(err_txt);
 				var element = document.getElementById(divid);
 				element.appendChild(para);
@@ -108,46 +102,30 @@ function get_data(url, json_query, dataId, divid) {
                           title
                           representation_hints
                         }
-                    }`
+                    }`,
 					});
 
-				fetchData().then(response => {
+				fetchData().then((response) => {
 					let metrics_list = response.data.getMetrics;
 
 					// iterate over the list of metrics to generate a dictionary
 					let metrics_names = {};
 					let metrics_representation = {};
-					metrics_list.forEach(function(element) {
+					metrics_list.forEach(function (element) {
 						metrics_names[element._id] = element.title;
 					});
 					// get optimization point
-					if (
-						result[0].datalink.inline_data.visualization
-							.optimization == 'bottom-right'
-					) {
+					if (result[0].datalink.inline_data.visualization.optimization == 'bottom-right') {
 						better[divid] = 'bottom-right';
 					} else {
 						better[divid] = 'top-right';
 					}
-					let metric_x =
-						result[0].datalink.inline_data.visualization.x_axis;
-					let metric_y =
-						result[0].datalink.inline_data.visualization.y_axis;
+					let metric_x = result[0].datalink.inline_data.visualization.x_axis;
+					let metric_y = result[0].datalink.inline_data.visualization.y_axis;
 					// append those metrics as div attributes, so that they cna be used later
-					document
-						.getElementById(divid)
-						.setAttribute('metric_x', metric_x);
-					document
-						.getElementById(divid)
-						.setAttribute('metric_y', metric_y);
-					join_all_json(
-						result,
-						divid,
-						metric_x,
-						metric_y,
-						metrics_names,
-						better
-					);
+					document.getElementById(divid).setAttribute('metric_x', metric_x);
+					document.getElementById(divid).setAttribute('metric_y', metric_y);
+					join_all_json(result, divid, metric_x, metric_y, metrics_names, better);
 				});
 			}
 		});
@@ -156,20 +134,11 @@ function get_data(url, json_query, dataId, divid) {
 	}
 }
 
-function join_all_json(
-	result,
-	divid,
-	metric_x,
-	metric_y,
-	metrics_names,
-	better
-) {
+function join_all_json(result, divid, metric_x, metric_y, metrics_names, better) {
 	try {
 		// transform the object to an array, which is usable by the D3 chart
 		let full_json = [];
-		result[0].datalink.inline_data.challenge_participants.forEach(function(
-			element
-		) {
+		result[0].datalink.inline_data.challenge_participants.forEach(function (element) {
 			//if participant name is too long, slice it
 			let tool_name = element.tool_id;
 			var short_name;
@@ -194,15 +163,7 @@ function join_all_json(
 		var e = document.getElementById(divid + '_dropdown_list');
 		let classification_type = e.options[e.selectedIndex].id;
 
-		createChart(
-			full_json,
-			divid,
-			classification_type,
-			metric_x,
-			metric_y,
-			metrics_names,
-			better
-		);
+		createChart(full_json, divid, classification_type, metric_x, metric_y, metrics_names, better);
 	} catch (err) {
 		console.log(`Invalid Url Error: ${err.stack} `);
 	}
@@ -214,15 +175,7 @@ function onQuartileChange(ID, metric_x, metric_y, better) {
 	d3.select('#' + 'svg_' + chart_id).remove();
 	let classification_type = ID;
 
-	createChart(
-		MAIN_DATA[chart_id],
-		chart_id,
-		classification_type,
-		metric_x,
-		metric_y,
-		MAIN_METRICS[chart_id],
-		better
-	);
+	createChart(MAIN_DATA[chart_id], chart_id, classification_type, metric_x, metric_y, MAIN_METRICS[chart_id], better);
 }
 
 export { load_scatter_visualization, onQuartileChange, better, downloadPng };
